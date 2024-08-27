ca3c98bc1bf6a64d071ea341b351e868b8652237
Timothy <timothywealth09@gmail.com>
2024-08-27 18:22:47 +0100

    Initial commit: Initialize project with package manager and server setup

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true, min: 2, max: 50 },
+  lastName: { type: String, required: true, min: 2, max: 50 },
+  email: { type: String, required: true, max: 50, unique: true },
+  password: { type: String, required: true, min: 5 },
+  profilePicture: { type: String, default: "default-profile.png" },
+  habitIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Habit" }],
+  taskIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+  vacationMode: { type: Boolean, default: false },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
